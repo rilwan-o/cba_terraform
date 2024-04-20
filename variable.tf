0972609cf308ddf10d9b27d7076798f84e030fb2
rilwan-o <opeayoriliwan@yahoo.com>
2024-04-20 15:35:16 +0100

    update terraform module

--- variable.tf
+++ variable.tf
@@ -6,14 +6,8 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "instance_ami" {
-  default = "ami-09cce85cf54d36b29"
-}
 
 
-variable "vpc_id" {
-  default = ""
-}
 
 
 variable "key_name" {
